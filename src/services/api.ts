b5e02af8a15e9600fff9e7460c65f429b520eb90
vgab1 <vgabriel.sousa123@gmail.com>
2025-04-17 12:02:17 -0300

    add mockapi

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // json-server --watch db.json
 export const api = axios.create({
-  baseURL: "https://680111cb81c7e9fbcc416972.mockapi.io",
+  baseURL: "https://680111cb81c7e9fbcc416972.mockapi.io/products",
 });
